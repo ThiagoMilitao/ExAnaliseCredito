383c5952507dddd288455c5c4b36db8e93549628
thiagoMilitao <thiago.militao2007@gmail.com>
2025-05-12 10:05:05 -0300

    ExAnaliseCredito

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Qual o valor do crédito?");
+double valorCredito = double.Parse(Console.ReadLine());
+Console.WriteLine("Qual a quantidade de parcelas desejadas?");
+double QtdParcela = double.Parse(Console.ReadLine());
+Console.WriteLine("Qual sua renda mensal?");
+double renda = double.Parse(Console.ReadLine());
+
+double prestacao = valorCredito/QtdParcela;
+
+if(prestacao >= (renda*0.3)){
+    Console.WriteLine("Seu crédito não foi autorizado");
+}else{
+    Console.WriteLine("Seu crédito foi aprovado");
+}
+
+
